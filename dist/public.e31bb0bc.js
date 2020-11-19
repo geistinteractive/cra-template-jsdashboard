1239b418db5783eba7b3e784586725747e8dd5b5
Jeremy Brown <jeremy@geistinteractive.com>
2020-11-19 10:17:05 -0500

    adding base info

--- dist/public.e31bb0bc.js
+++ dist/public.e31bb0bc.js
@@ -118,8 +118,66 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 
   return newRequire;
 })({"index.js":[function(require,module,exports) {
+function print_filter(filter) {
+  var f = eval(filter);
 
-},{}],"../../../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+  if (typeof f.length != "undefined") {} else {}
+
+  if (typeof f.top != "undefined") {
+    f = f.top(Infinity);
+  } else {}
+
+  if (typeof f.dimension != "undefined") {
+    f = f.dimension(function (d) {
+      return "";
+    }).top(Infinity);
+  } else {}
+
+  console.log(filter + "(" + f.length + ") = " + JSON.stringify(f).replace("[", "[\n\t").replace(/}\,/g, "},\n\t").replace("]", "\n]"));
+}
+
+var dayFormat = d3.timeFormat("%d");
+var numFormatLg = d3.format("~s");
+var numFormat = d3.format(",");
+var monthName = d3.timeFormat("%B");
+var monthFormat = d3.timeFormat("%m");
+
+function remove_empty_bins(source_group) {
+  return {
+    all: function all() {
+      return source_group.all().filter(function (d) {
+        return d.value != 0;
+      });
+    }
+  };
+}
+
+window.loadData = function (json) {
+  var data = JSON.parse(json);
+  console.log(data);
+
+  var updateObjects = function updateObjects(d, i) {
+    //basic items. Remove if necessary
+    d.Date = new Date(d.fieldData.Date);
+    d.Year = d.Date.getFullYear(); // d.Day = dayFormat(currDate);
+    // d.Month = currDate.getMonth();
+  };
+
+  data.forEach(updateObjects);
+  var facts = crossfilter(data);
+  var companiesKey = "Companies::Name";
+  var companiesDim = facts.dimension(function (d) {
+    return d.fieldData[companiesKey];
+  });
+  var companiesGroup = companiesDim.group();
+  print_filter(companiesGroup);
+  window.companiesBarChart = dc.barChart("#companiesBarChart");
+  companiesBarChart.group(companiesGroup).dimension(companiesDim).renderLabel(true).clipPadding(100).height(400).x(d3.scaleBand()).xUnits(dc.units.ordinal).colorAccessor(function (d, i) {
+    return i;
+  }).elasticX(true).elasticY(true).ordinalColors(["#001EBA", "#05147D", "#35459E", "#003B74", "#0B42FF"]);
+  dc.renderAll();
+};
+},{}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -147,7 +205,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53893" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51049" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -323,5 +381,5 @@ function hmrAcceptRun(bundle, id) {
     return true;
   }
 }
-},{}]},{},["../../../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js","index.js"], null)
+},{}]},{},["../node_modules/parcel-bundler/src/builtins/hmr-runtime.js","index.js"], null)
 //# sourceMappingURL=/public.e31bb0bc.js.map
---
+++ public/index.js
@@ -0,0 +1,83 @@
+function print_filter(filter) {
+  var f = eval(filter);
+  if (typeof f.length != "undefined") {
+  } else {
+  }
+  if (typeof f.top != "undefined") {
+    f = f.top(Infinity);
+  } else {
+  }
+  if (typeof f.dimension != "undefined") {
+    f = f
+      .dimension(function (d) {
+        return "";
+      })
+      .top(Infinity);
+  } else {
+  }
+  console.log(
+    filter +
+      "(" +
+      f.length +
+      ") = " +
+      JSON.stringify(f)
+        .replace("[", "[\n\t")
+        .replace(/}\,/g, "},\n\t")
+        .replace("]", "\n]")
+  );
+}
+var dayFormat = d3.timeFormat("%d");
+var numFormatLg = d3.format("~s");
+var numFormat = d3.format(",");
+var monthName = d3.timeFormat("%B");
+var monthFormat = d3.timeFormat("%m");
+
+function remove_empty_bins(source_group) {
+  return {
+    all: function () {
+      return source_group.all().filter(function (d) {
+        return d.value != 0;
+      });
+    },
+  };
+}
+
+window.loadData = function (json) {
+  var data = JSON.parse(json);
+  console.log(data);
+  var updateObjects = function (d, i) {
+    //basic items. Remove if necessary
+    d.Date = new Date(d.fieldData.Date);
+    d.Year = d.Date.getFullYear();
+    // d.Day = dayFormat(currDate);
+    // d.Month = currDate.getMonth();
+  };
+
+  data.forEach(updateObjects);
+  var facts = crossfilter(data);
+
+  var companiesKey = "Companies::Name";
+  var companiesDim = facts.dimension(function (d) {
+    return d.fieldData[companiesKey];
+  });
+  var companiesGroup = companiesDim.group();
+  print_filter(companiesGroup);
+  window.companiesBarChart = dc.barChart("#companiesBarChart");
+
+  companiesBarChart
+    .group(companiesGroup)
+    .dimension(companiesDim)
+    .renderLabel(true)
+    .clipPadding(100)
+    .height(400)
+    .x(d3.scaleBand())
+    .xUnits(dc.units.ordinal)
+    .colorAccessor(function (d, i) {
+      return i;
+    })
+    .elasticX(true)
+    .elasticY(true)
+    .ordinalColors(["#001EBA", "#05147D", "#35459E", "#003B74", "#0B42FF"]);
+
+  dc.renderAll();
+};
